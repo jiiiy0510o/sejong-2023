5e772562a036baff28d9f0f408a05836c8fda300
Young <jiiiy0510o@gmail.com>
2023-12-28 23:42:30 +0900

    슬라이드 수정 및 TOP 버튼

--- js/index.js
+++ js/index.js
@@ -18,17 +18,45 @@ let sliderMark;
 let i = 1;
 
 function showInterval() {
-  imgIndex = document.querySelector(`.imgBox > img:nth-child(${i})`);
+  // 현재 이미지와 슬라이더 마크에서 클래스 제거
+  imgIndex = document.querySelector(`.imgBox > img.showImg`);
+  sliderMark = document.querySelector(`.slider > div.mark`);
+  imgIndex.classList.remove("showImg");
+  sliderMark.classList.remove("mark");
 
-  sliderMark = document.querySelector(`.slider > div:nth-child(${i})`);
-
-  console.log(imgIndex);
-  imgIndex.classList.add("showImg");
-  sliderMark.classList.add("mark");
   i++;
-  if (i > 4) {
+
+  if (i > 3) {
     i = 1;
   }
+
+  // i 값에 따라 새로운 imgIndex와 sliderMark를 설정합니다.
+  imgIndex = document.querySelector(`.imgBox > img:nth-child(${i})`);
+  sliderMark = document.querySelector(`.slider > div:nth-child(${i})`);
+
+  // 새 이미지와 슬라이더 마크에클래스 추가
+  imgIndex.classList.add("showImg");
+  sliderMark.classList.add("mark");
 }
 showInterval();
 setInterval(showInterval, 3000);
+
+//TOP버튼 클릭시 상단 이동
+let topBtn = document.querySelector(".topBtn");
+
+topBtn.addEventListener("click", () => {
+  //동작을 부드럽게
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", (e) => {
+  let scrollY = this.scrollY;
+  if (scrollY > 100) {
+    topBtn.style.bottom = "40%";
+    topBtn.style.transition = "bottom 0.5s ease-out";
+  } else {
+    // 스크롤 위치가 100 이하일 경우 TOP 버튼 숨김
+    topBtn.style.bottom = "-100px";
+    topBtn.style.transition = "bottom 0.5s ease-out";
+  }
+});
