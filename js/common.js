690772c319039bddc9cf469eb31ae0850b672401
Young <jiiiy0510o@gmail.com>
2024-01-30 23:57:06 +0900

    nav 수정

--- js/common.js
+++ js/common.js
@@ -2,3 +2,9 @@
 $("header").load("/components/common/header.html");
 $("footer").load("/components/common/footer.html");
 $(".sideBtn").load("/components/common/sideBtn.html");
+
+let selected = document.querySelector(".selected");
+let ulList = document.querySelector(".select ul");
+selected.addEventListener("mouseenter", function () {
+  ulList.style.display = "block";
+});
