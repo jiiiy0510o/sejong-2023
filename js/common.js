ec6dcbb96565ff09e6d418ec916cf51a66e99cac
Young <jiiiy0510o@gmail.com>
2024-01-31 23:59:52 +0900

    nav css, js 수정

--- js/common.js
+++ js/common.js
@@ -3,8 +3,29 @@ $("header").load("/components/common/header.html");
 $("footer").load("/components/common/footer.html");
 $(".sideBtn").load("/components/common/sideBtn.html");
 
-let selected = document.querySelector(".selected");
-let ulList = document.querySelector(".select ul");
-selected.addEventListener("mouseenter", function () {
-  ulList.style.display = "block";
+let allSelect = document.querySelectorAll(".select");
+
+allSelect.forEach(function (select) {
+  let ulList = select.querySelector("ul");
+  let selected = select.querySelector(".selected");
+  select.addEventListener("click", function () {
+    allSelect.forEach(function (otherSelect) {
+      let otherUlList = otherSelect.querySelector("ul");
+      let otherSelected = otherSelect.querySelector(".selected");
+      if (otherUlList !== ulList) {
+        otherSelected.style.backgroundColor = "";
+        otherSelected.style.color = "";
+        otherUlList.style.display = "none";
+      }
+    });
+    if (ulList.style.display === "block") {
+      selected.style.backgroundColor = "";
+      selected.style.color = "";
+      ulList.style.display = "none";
+    } else {
+      selected.style.backgroundColor = "#299a52";
+      selected.style.color = "#fff";
+      ulList.style.display = "block";
+    }
+  });
 });
